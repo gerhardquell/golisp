f3dd449c73376cae7b5846434e8ad3b51f07d165
Gerhard Quell <gquell@skequell.de>
2026-02-24 13:58:44 +0100

    test: swap!-Makro demonstriert hygienisches gensym

--- testlib.lisp
+++ testlib.lisp
@@ -7,3 +7,15 @@
 ; TCO-Test: tiefe Rekursion
 (defun countdown (n) (if (= n 0) "done" (countdown (- n 1))))
 (println (countdown 1000000))
+
+; gensym-Demo: swap!-Makro ohne Variablenkonflikt
+(defmacro swap! (a b)
+  (let ((tmp (gensym)))
+    `(let ((,tmp ,a))
+       (begin (set! ,a ,b) (set! ,b ,tmp)))))
+
+(define x 10)
+(define y 20)
+(swap! x y)
+(println x)   ; soll 20 ausgeben
+(println y)   ; soll 10 ausgeben
