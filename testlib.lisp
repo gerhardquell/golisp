4fdb94da2a9dc6da0446d6f286e7405b52854a11
Gerhard Quell <gquell@skequell.de>
2026-02-24 13:35:07 +0100

    test: Tiefrekursions-Test (countdown 1000000) für TCO-Verifikation

--- testlib.lisp
+++ testlib.lisp
@@ -3,3 +3,7 @@
 (defun kubik (n) (* n n n))
 (defun inc (n) (+ n 1))
 (defun dec (n) (- n 1))
+
+; TCO-Test: tiefe Rekursion
+(defun countdown (n) (if (= n 0) "done" (countdown (- n 1))))
+(println (countdown 1000000))
